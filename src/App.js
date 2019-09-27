560b5656109778d6b1904b747dd84c8fb7cd7008
Khushboo Taneja <tankh01@ca.com>
2019-09-27 13:23:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+//import React from 'react';
+import React, { Component } from 'react'
+import './App.css';
+import CardComponent from './components/CardComponent';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount(){
+    /* fetch('https://api.yelp.com/v3/businesses/search?location="Alpharetta"', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer' + 'Ubup8E7BuzVHBGAmhAHSS17fVR_DpoVr4FMDTcwJeh6iqVzAkLYqSTBXWpN_jg2yB0r400fdQmKkkX8P5mfcK8VsgE5ykIMye9Us_79YC7gMLLkotEvKf0o0dGGMXXYx',
+        //'mode': 'no-cors'
+      }
+    })
+    .then( res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(console.log) */
+    fetch('/api/getdata').then(res => res.json()).then(data => {
+      console.log(data);
+      this.setState({data:data.result});
+    })
+  }
+
+  render(){
+    return(
+      <CardComponent cardLists={this.state.data}/>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/CardComponent.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Card } from 'antd';
+import 'antd/lib/card/style/css';
+import '../stylesheets/App.css';
+
+const { Meta } = Card;
+
+class CardComponent extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+      const cardStyle = {
+        textAlign: 'center'
+      };
+      const headStyle = {
+        fontSize: '30px',
+        fontStyle: 'italic',
+        fontFamily: 'monospace'
+      }
+    const gridStyle = {
+      width: '30%',
+      height: '30%',
+      textAlign: 'left'
+    };
+    const imgStyle = {
+        maxHeight: '250px'
+    }
+    const metaStyle = {
+        paddingTop: '5px',
+        paddingBottom: '5px'
+    }
+    const paddingTop = {
+        paddingTop: '3px'
+    }
+    return (
+      <Card style={cardStyle}>
+        <h1 style={headStyle}>Top five ice-cream parlors in your town</h1>
+      {this.props.cardLists.map( item => (
+      <Card.Grid style={gridStyle} key={item.id}>
+      <Card
+        hoverable
+        cover={<img alt={item.name} src={item.imageUrl} style={imgStyle}/>}
+      >
+      <Meta style={metaStyle} title={item.name} description={`${item.street},${item.city}`} />
+    </Card>
+    <h3 style={paddingTop}>TOP REVIEWS</h3>
+    <p>$<b>{item.review[0].userName}</b> says "${item.review[0].text}"</p>
+    <p>$<b>{item.review[1].userName}</b> says "${item.review[1].text}"</p>
+    <p>$<b>{item.review[2].userName}</b> says "${item.review[2].text}"</p>
+    </Card.Grid>
+  ))}
+  </Card>
+  );
+  }
+}
+
+export default CardComponent;
